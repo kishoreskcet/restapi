fb6a017c013d52bef9a48ca1bc52752732efc25f
kishoreskcet <108710342+kishoreskcet@users.noreply.github.com>
2023-08-22 15:47:08 +0530

    Delete day3jpa-1 directory

--- day3jpa-1/src/main/java/com/day4/repo/repo.java
+++
@@ -1,9 +0,0 @@
-package com.day4.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.day4.model.model;
-
-public interface repo extends JpaRepository<model, Integer>{
-
-}

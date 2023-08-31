e2046f2a8e8233d2d6ef93d6cee9c0c5279f09b9
kishoreskcet <108710342+kishoreskcet@users.noreply.github.com>
2023-08-31 09:24:29 +0530

    Add files via upload

---
+++ bookingsystem/src/main/java/com/example/day5/repo/userrepo.java
@@ -0,0 +1,20 @@
+package com.example.day5.repo;
+
+//import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.day5.model.userdetails;
+//import com.example.day7.model.model;
+//import com.example.day7.model.model;
+
+//import jakarta.transaction.Transactional;
+
+public interface userrepo extends JpaRepository<userdetails, Integer>{
+	
+
+	
+}package com.example.day5.repo;
+
+public interface userrepo {
+
+}
